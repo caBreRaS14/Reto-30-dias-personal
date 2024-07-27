a7daafa4266ff805ae57272f775c186c5bd84422
caBreRaS14 <97133878+caBreRaS14@users.noreply.github.com>
2024-07-26 22:19:43 -0500

    una prueba de como se podria realizar el cidigo pendiente a modificaciones

--- El-partido-tennis/partido.js
+++ El-partido-tennis/partido.js
@@ -1,1 +1,28 @@
-alert("Hola")
+let jugador1 = 0;
+let jugador2 =0;
+
+let marcador = true;
+
+function partido (){
+    while(marcador){
+        let puntaje = Number(prompt("¡quien marca punto?"));
+        if(puntaje === 1){
+            jugador1 = jugador1 + 15; 
+            
+        }else{
+            if(jugador2 === 0){
+                jugador2 = "love";
+            }else if (jugador2 ===30){
+                Number(jugador2) = jugador2 + 10;
+            }else{
+                jugador2 =+ 15; 
+
+            }
+            
+            
+        }
+        console.log(jugador1 +" - "+ jugador2);
+    }
+}
+
+partido()
